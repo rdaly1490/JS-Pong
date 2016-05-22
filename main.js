3032fd5864e7f315274dffe0643ac18bebc6db55
Rob <rdaly1490@gmail.com>
2016-05-22 16:41:03 -0500

    got a bouncing ball woohoo

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 5;
 
 window.onload = function() {
 
 canvas = document.getElementById('game-canvas');
 canvasContext = canvas.getContext('2d');
 
-setInterval(drawEverything, 1000);
+var framesPerSecond = 30;
+setInterval(function() {
+	moveEverything();
+	drawEverything();
+}, 1000/framesPerSecond);
 
 
 
@@ -16,11 +21,21 @@ setInterval(drawEverything, 1000);
 
 }
 
+function moveEverything() {
+	ballX = ballX + ballSpeedX;
+	// check bounds of game ball
+	if (ballX > canvas.width) {
+		ballSpeedX = -ballSpeedX; 
+	} else if (ballX < 0) {
+		ballSpeedX = -ballSpeedX;
+	}
+}
+
 function drawEverything() {
-	ballX = ballX + 20;
-	console.log(ballX);
 	canvasContext.fillStyle = 'black';
 	canvasContext.fillRect(0,0, canvas.width, canvas.height);
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(10, 210, 10, 100);
 	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(ballX, 200, 50, 50);
+	canvasContext.fillRect(ballX, 100, 10, 10);
 }
